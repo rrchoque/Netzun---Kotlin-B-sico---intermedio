8b26c709217dc534e66f1053f5dd3b56a1cd26e0
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-12-08 01:02:00 -0400

    Variables de tipo texto y número

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,4 +10,23 @@ fun main() {
     // Inferencia de tipos
     var saludos = "Hola mundo!"
     println(saludos)
+
+    // Variables de tipo texto
+    var nombre = "Peter"
+    var apellido = "Parker"
+    println(nombre + " " + apellido)
+    println("$nombre $apellido")
+
+    // Variables de tipo número
+    var numero1 = 7
+    var numero2 = 2
+    // Operaciones matemáticas
+    println("Suma: ${numero1 + numero2}")
+    println("Resta: ${numero1 - numero2}")
+    println("Multiplicación: ${numero1 * numero2}")
+    println("División: ${numero1 / numero2}")
+    println("Módulo: ${numero1 % numero2}")
+
+    // Nullables en kotlin
+    
 }
