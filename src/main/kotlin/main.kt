4c31ba660512c5d86b739d412027e1a8d7c43670
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-12-12 08:41:05 -0400

    Se completó el módulo de loops

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,53 @@
 fun main() {
-    // Creando una lista
-    // listOf(value1, value2, value3, etc..)
-    // var/var listName = listOf(value1, value2, value3, etc..)
-    // listOf (inmutable) : mutableListOf (mutable)
-    var paises1 = mutableListOf("Bolivia", "Argentina", "Peru")
-    println(paises1)
-    println(paises1[2])
+    // Loops
+    // For
+    for (i in 1..4){
+        println("Hola mundo")
+    }
 
-    paises1[2] = "Brasil"
-    println(paises1[2])
+    // Iteración de colección
+    var listaPersonajes = listOf("Spiderman", "Wolverine", "Ironman")
+    for ((indice, personaje) in listaPersonajes.withIndex()) {
+        println("$indice: $personaje")
+    }
 
-    // Crear un Set (No admite duplicados)
-    // setOf (inmutable) : mutableSetOf (mutable)
-    var paises = mutableSetOf("Bolivia", "Argentina", "Peru", "Bolivia")
-    println(paises)
-    println(paises.elementAt(2))
-    paises.remove("Peru")
-    println(paises)
+    // Iteración de Maps
+    var user = mutableMapOf("nombre" to "Reynaldo", "pass" to "123")
+    for (item in user) {
+        println("${item.key} : ${item.value}")
+    }
+    for ((key, value) in user) {
+        println("$key : $value")
+    }
 
-    // Crear un Map
-    // MapOf (inmutable) : mutableMapOf (mutable)
-    var datosUsuario = mutableMapOf("nombre" to "Reynaldo", "pass" to "123")
-    println(datosUsuario)
-    println(datosUsuario["nombre"])
-    println(datosUsuario.keys)
-    println(datosUsuario.values)
-    datosUsuario["pass"] = "456"
-    println(datosUsuario)
+    // While Loop
+    var num = 1
+    while (num < 4) {
+        println("Esta es una línea")
+        num = num + 1
+    }
 
+    // do..While Loop
+    var suerte = 10
+    do {
+        println("Ingrese su número:")
+        var numero = readLine()!!.toInt()
+        if (numero > suerte) {
+            println("Tu número es muy alto")
+        }
+        if (numero < suerte) {
+            println("Tu número es muy bajo")
+        }
+        if (numero == suerte) {
+            println("Ganaste!!")
+        }
+    } while (suerte != numero)
+
+    // Loops anidados
+    for (i in 1..4) {
+        println("fila: $i")
+        for (j in 1..4){
+            println("$j")
+        }
+    }
 }
