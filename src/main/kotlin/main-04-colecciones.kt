70de04b503c58dc93fb358d3cc2d8f7c18a883a9
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-12-12 08:16:25 -0400

    Se completó el comntenido correcto de la sección

--- src/main/kotlin/main-04-colecciones.kt
+++ src/main/kotlin/main-04-colecciones.kt
@@ -1,4 +1,31 @@
 fun main() {
-    // 
+    // Creando una lista
+    // listOf(value1, value2, value3, etc..)
+    // var/var listName = listOf(value1, value2, value3, etc..)
+    // listOf (inmutable) : mutableListOf (mutable)
+    var paises1 = mutableListOf("Bolivia", "Argentina", "Peru")
+    println(paises1)
+    println(paises1[2])
+
+    paises1[2] = "Brasil"
+    println(paises1[2])
+
+    // Crear un Set (No admite duplicados)
+    // setOf (inmutable) : mutableSetOf (mutable)
+    var paises = mutableSetOf("Bolivia", "Argentina", "Peru", "Bolivia")
+    println(paises)
+    println(paises.elementAt(2))
+    paises.remove("Peru")
+    println(paises)
+
+    // Crear un Map
+    // MapOf (inmutable) : mutableMapOf (mutable)
+    var datosUsuario = mutableMapOf("nombre" to "Reynaldo", "pass" to "123")
+    println(datosUsuario)
+    println(datosUsuario["nombre"])
+    println(datosUsuario.keys)
+    println(datosUsuario.values)
+    datosUsuario["pass"] = "456"
+    println(datosUsuario)
 
 }
